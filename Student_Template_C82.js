81da7ff79c533a257e523dee221fdfbcb55df8b6
Stacey14022011 <123970177+Stacey14022011@users.noreply.github.com>
2023-04-03 21:07:27 +0530

    Add files via upload

---
+++ Student_Template_C82.js
@@ -0,0 +1,22 @@
+/*
+Concatenate the first name and the last name to print the full name of the person on the console. 
+Type the line: "first_name.concat(last_name)"
+*/       
+/*
+Create a variable named add and store the addition value of a and b in that variable.
+Type: var add = a + b
+Then, print the variable to display the addition value
+Type: console.log(add)
+*/
+/*
+Create a variable named sub and store the subtraction value of a and b in that variable.
+Type: var sub = a - b
+Then, print the variable to display the subtraction value
+Type: console.log(sub)
+*/
+/*
+Create a variable named multiple and store the multiplication value of a and b in that variable.
+Type: var multiple = a * b
+Then, print the variable to display the multiplication value
+Type: console.log(multiple)
+*/
